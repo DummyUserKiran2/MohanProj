0cb8bd329dd7d44cb225b2e971c23171e14aa7c8
mohan k <mohan4py@gmail.com>
2022-09-01 18:20:25 -0600

    SecondProg

---
+++ MohanAutomationProj/src/test/java/com/seleniumprograms/SecondProg.java
@@ -0,0 +1,12 @@
+package com.seleniumprograms;
+
+import org.testng.annotations.Test;
+
+public class SecondProg {
+
+	@Test
+	public void m1() {
+		System.out.println("This is m1 method");
+	}
+
+}
